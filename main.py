56e75eba28088139061960a30c1baad30c6e608f
Billit Shotta <billitshotta@gmail.com>
2023-05-07 11:05:23 +0700

    adding malam 40 100 1000

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import datetime
 import json
 import subprocess
 
-hariMeninggal = datetime.datetime(2023,5,7,18)
+hariMeninggal = datetime.datetime(2022,8,21,10)
 
 print(hariMeninggal.strftime('%d %m %Y %H:%M:%S'))
 
@@ -16,7 +16,8 @@ elif int(hariMeninggal.strftime('%H')) >17 and int(hariMeninggal.strftime('%H'))
     print("Dianta jam 5 dan jam 7 malam")
     
     now = datetime.datetime.now()
-    current_date = now.strftime("%Y/%m/%d")
+    #current_date = now.strftime("%Y/%m/%d")
+    current_date = hariMeninggal.strftime("%Y/%m/%d") #modif
     jakarta_city_code = "1301"
     result = subprocess.run(["curl", f"https://api.myquran.com/v1/sholat/jadwal/{jakarta_city_code}/{current_date}"],
                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -38,14 +39,35 @@ elif int(hariMeninggal.strftime('%H')) >17 and int(hariMeninggal.strftime('%H'))
     waktuMaghrib = waktuMaghrib.split(':')
     print(waktuMaghrib)
 
-    
+    #apakah sesudah maghrib?
+    if int(hariMeninggal.strftime("%H")) == int(waktuMaghrib[0]):
+        print("Waktu sama di lanjut pengecekan menit")
+        #Pengecekan menit
+        if int(hariMeninggal.strftime("%M")) >= int(waktuMaghrib[1]):
+            print("Setelah waktu maghrib")
+            penjumlahanHari = 1
+    elif int(hariMeninggal.strftime("%H")) > int(waktuMaghrib[0]):
+        print("Setelah waktu maghrib")
+        penjumlahanHari = 1
+    else:
+        print("Kurang dari waktu maghrib")
+
+
 else:
     penjumlahanHari = 1
     print("Di atas jam 7 malam")
 
 malamPertama =  hariMeninggal + datetime.timedelta(days=penjumlahanHari)
+malamKedua = malamPertama + datetime.timedelta(days=1)
+malamKeTujuh = malamPertama +datetime.timedelta(days=5)
+malamKeEmpatPuluh = malamPertama +datetime.timedelta(days=38)
+malamKeSeratus = malamPertama +datetime.timedelta(days=98)
+malamKeSeribu = malamPertama +datetime.timedelta(days=998)
 
-
-print("Malam pertama yaitu", malamPertama)
-print("Malam kedua yaitu")
+print("Malam 1 yaitu", malamPertama.strftime("%d %B %Y"))
+print("Malam 2 yaitu", malamKedua.strftime("%d %B %Y"))
+print("Malam 7 yaitu", malamKeTujuh.strftime("%d %B %Y"))
+print("Malam 40 yaitu", malamKeEmpatPuluh.strftime("%d %B %Y"))
+print("Malam 100 yaitu", malamKeSeratus.strftime("%d %B %Y"))
+print("Malam 1000 yaitu", malamKeSeribu.strftime("%d %B %Y"))
 
