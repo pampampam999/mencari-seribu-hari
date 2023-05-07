11aa832c8c2d24a6604c26a9a7d632b252cfcd36
Billit Shotta <billitshotta@gmail.com>
2023-05-07 09:58:17 +0700

    waktu maghrip from string to int

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ if int(hariMeninggal.strftime('%H')) < 17:
     print("Sebelum Jam Lima")
 elif int(hariMeninggal.strftime('%H')) >17 and int(hariMeninggal.strftime('%H')) < 19:
     print("Dianta jam 5 dan jam 7 malam")
+    
     now = datetime.datetime.now()
     current_date = now.strftime("%Y/%m/%d")
     jakarta_city_code = "1301"
@@ -21,17 +22,23 @@ elif int(hariMeninggal.strftime('%H')) >17 and int(hariMeninggal.strftime('%H'))
                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     result_json = json.loads(result.stdout)
-    print(result_json["data"]["lokasi"])
-    print(result_json["data"]["daerah"])
-    print(f'Hari/Tanggal: {result_json["data"]["jadwal"]["tanggal"]}')
-    print(f'- imsak {result_json["data"]["jadwal"]["imsak"]}')
-    print(f'- subuh {result_json["data"]["jadwal"]["subuh"]}')
-    print(f'- terbit {result_json["data"]["jadwal"]["terbit"]}')
-    print(f'- dhuha {result_json["data"]["jadwal"]["dhuha"]}')
-    print(f'- dzuhur {result_json["data"]["jadwal"]["dzuhur"]}')
-    print(f'- ashar {result_json["data"]["jadwal"]["ashar"]}')
-    print(f'- maghrib {result_json["data"]["jadwal"]["maghrib"]}')
-    print(f'- isya {result_json["data"]["jadwal"]["isya"]}')
+    # print(result_json["data"]["lokasi"])
+    # print(result_json["data"]["daerah"])
+    # print(f'Hari/Tanggal: {result_json["data"]["jadwal"]["tanggal"]}')
+    # print(f'- imsak {result_json["data"]["jadwal"]["imsak"]}')
+    # print(f'- subuh {result_json["data"]["jadwal"]["subuh"]}')
+    # print(f'- terbit {result_json["data"]["jadwal"]["terbit"]}')
+    # print(f'- dhuha {result_json["data"]["jadwal"]["dhuha"]}')
+    # print(f'- dzuhur {result_json["data"]["jadwal"]["dzuhur"]}')
+    # print(f'- ashar {result_json["data"]["jadwal"]["ashar"]}')
+    # print(f'- maghrib {result_json["data"]["jadwal"]["maghrib"]}') #Ini saja yang  di pakai
+    # print(f'- isya {result_json["data"]["jadwal"]["isya"]}')
+
+    waktuMaghrib = result_json["data"]["jadwal"]["maghrib"]
+    waktuMaghrib = waktuMaghrib.split(':')
+    print(waktuMaghrib)
+
+    
 else:
     penjumlahanHari = 1
     print("Di atas jam 7 malam")
